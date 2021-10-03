22be5bab5f27afd1a2320c3cd6e1f35605f40535
alencarable <alencarable@aol.com>
2021-10-03 11:35:20 -0300

    tipos em js foi adicionado

---
+++ ES6Essencial/tipos/null.js
@@ -0,0 +1,7 @@
+const nullVariable = null
+console.log(typeof nullVariable);
+
+/*
+O retorno desse console.log será object
+Contudo, se for colocado para ser impresso apenas a variável, então retornará null.
+*/
